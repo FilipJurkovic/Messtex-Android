10b4e6686bbc441a233fe0fdaaa3b7fbec64be19
Filip Jurković <filip@brightdock.com>
2021-05-19 16:15:03 +0200

    Implemented ViewPager adapter

---
+++ app/src/main/java/com/messtex/ui/main/adapter/MainActivityAdapter.kt
@@ -0,0 +1,24 @@
+package com.messtex.ui.main.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+
+class MainActivityAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+    private val fragmentList: MutableList<Fragment> = ArrayList()
+    private val titleList: MutableList<String> = ArrayList()
+
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+
+    fun addFragment(fragment: Fragment, title: String){
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+}
--- app/src/main/java/com/messtex/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/messtex/ui/main/view/MainActivity.kt
@@ -3,10 +3,19 @@ package com.messtex.ui.main.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.messtex.R
+import com.messtex.ui.fragments.view.StepMeterInfo
+import com.messtex.ui.fragments.view.StepUserInfo
+import com.messtex.ui.main.adapter.MainActivityAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = MainActivityAdapter(supportFragmentManager)
+        adapter.addFragment(StepUserInfo(), "UserInfo")
+        adapter.addFragment(StepMeterInfo(), "MeterInfo")
+        viewPager.adapter = adapter
     }
 }
